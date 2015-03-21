99fab1bf67e9b2b778b58d017cea7b6b85b75518
Ryan Popa <ryanp@recsolu.com>
2015-03-21 14:49:30 -0500

    Got a working version of the also

--- src/doll_smuggler/core.clj
+++ src/doll_smuggler/core.clj
@@ -5,3 +5,14 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn m
+  "The max value attained with weight less than or equal to w using items up to i"
+  [i w weight v]
+  (if (= i 0)
+    0
+    (if (> (nth w (dec i)) weight) (m (dec i) w weight v)
+      (max (m (dec i) w weight v) (+ (m (dec i) w (- weight (nth w (dec i))) v) (nth v (dec i))))
+    )
+  )
+)
