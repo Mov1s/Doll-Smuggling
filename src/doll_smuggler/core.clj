1d85f15631d50a08277de5d2b854152c6e74d9f9
Ryan Popa <ryanp@recsolu.com>
2015-03-22 14:11:49 -0500

    Adding final solution functions and tests

--- src/doll_smuggler/core.clj
+++ src/doll_smuggler/core.clj
@@ -1,17 +1,14 @@
 (ns doll-smuggler.core
   (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!")
-  (println "Test")
-)
+(use 'clojure.pprint)
 
 (declare mm)
 
 (defn m
-  "The max value attained with weight less than or equal to w using items up to i"
+  "The max value attained with weight less than or equal to w using items up to i
+   This expects dolls to be formatted and it will return a list of the following format:
+   (399 [1 2 3 4 10]) Where the first item is the total weight of the chosen dolls and the
+   second item is the indexes of the dolls chosen to get that weight."
   [i w dolls]
   (
     ;If you try to find the max weight of 0 items it will always be 0, :P
@@ -27,7 +24,7 @@
               [vy yindexes] (mm (dec i) (- w wi) dolls)]
 
           ;If the max value acheivable by taking this doll is greater than the max value achievable without taking this doll
-	  ;return the max value for this position (i) and add i to the list of taken indexes
+          ;return the max value for this position (i) and add i to the list of taken indexes
           (if (> (+ vy vi) vn) [(+ vy vi) (conj yindexes  i)]
 
             ;Otherwise take the max value without this doll and continue the recurrsion
@@ -39,20 +36,57 @@
   )
 )
 
+;Neat little trick I found online for cacheing the results of a method when the same arguments are used
+;brought this recursive function from unbearably slow to pretty quick
 (def mm (memoize m))
 
-(defn formatdolls
+(defn format-dolls
   "Turns a list of doll info into a list of dictionaries of dolls"
   [dolls]
   (map (fn [[name weight value]] {:name name :weight weight :value value}) (partition 3 dolls))
 )
 
+(defn filter-by-index
+  "Takes a list and creates a new list using only the given indexes"
+  [items indexes]
+  (map (partial nth items) indexes))
+
 (defn knapsack
-  "Finds the best combination of dolls to give the most value for a max weight"
+  "Takes a formatted list of dolls and the max weight the old lady can carry and
+   finds the best combination of dolls to give the most value for that weight.
+   Returns a formatted list of dolls."
   [maxWeight dolls]
-  (let [formatteddolls (formatdolls dolls)
-        dollcount (dec (count formatteddolls))]
-    (m dollcount maxWeight formatteddolls)
+  (let [dollcount (dec (count dolls)) solution (m dollcount maxWeight dolls)]
+    (filter-by-index dolls (nth solution 1))
   )
 )
 
+(defn knapsack-unformatted
+  "Takes an unformatted list of dolls and the max weight the old lady can carry and
+   finds the best combination of dolls to give the most value for that weight.
+   Returns a formatted list of dolls."
+   [maxWeight dolls]
+   (knapsack maxWeight (format-dolls dolls))
+)
+
+(defn print-problem
+  "Takes a formatted list of dolls, a formatted solution, and a max weight and prints the entire problem
+   in an easily readable format."
+  [maxWeight dolls solution]
+  (println (str "\nMax weight: " maxWeight))
+  (println "\nAvailable dolls:")
+  (print-table dolls)
+  (println "\n\nPacked dolls:")
+  (print-table solution)
+)
+
+(defn -main
+  "Shortcut for using knapsack-unformatted and print-problem"
+  [maxWeight dolls]
+  (
+    (let [formattedDolls (format-dolls dolls)
+          solution (knapsack maxWeight formattedDolls)]
+      (print-problem maxWeight formattedDolls solution)
+    )
+  )
+)
--- test/doll_smuggler/core_test.clj
+++ test/doll_smuggler/core_test.clj
@@ -2,53 +2,101 @@
   (:require [clojure.test :refer :all]
             [doll-smuggler.core :refer :all]))
 
+
+
 (deftest formatdolls-test
   (testing "Format doll info into doll list."
     (def dolls '( luke 9 150
-    		  anthony 13 35))
+                  anthony 13 35))
     (def formatted '( {:name luke :weight 9 :value 150}
                       {:name anthony :weight 13 :value 35}))
-    (is (= (formatdolls dolls) formatted))))
+    (is (= (format-dolls dolls) formatted))))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (def dolls '( luke 9 150
-  	  	  anthony 13 35
-		  candice 153 200
-   		  dorothy 50 160
-                  puppy 15 60
-                  thomas 68 45
-                  randal 27 60
-                  april 39 40
-                  nancy 23 30
-                  bonnie 52 10
-                  marc 11 70
-                  kate 32 30
-                  tbone 24 15
-                  tranny 48 10
-                  uma 73 40
-                  grumpkin 42 70
-                  dusty 43 75
-                  grumpy 22 80
-                  eddie 7 20
-                  tory 18 12
-                  sally 4 50
-                  babe 30 10))
 
-    (def solution '( sally 4 50
-                     eddie 7 20
- 		     luke 9 150
-                     marc 11 70
-                     anthony 13 35
-                     puppy 15 60
-		     grumpy 22 80
-		     randal 27 60
- 		     grumpkin 42 70
-		     dusty 43 75
- 		     dorothy 50 160
-		     candice 153 200))
+
+(deftest solution-test
+  (testing "Finds the optimal doll combination for a given weight"
+    (def dolls (format-dolls '( luke 9 150
+                	  	          anthony 13 35
+              		              candice 153 200
+                                dorothy 50 160
+                                puppy 15 60
+                                thomas 68 45
+                                randal 27 60
+                                april 39 40
+                                nancy 23 30
+                                bonnie 52 10
+                                marc 11 70
+                                kate 32 30
+                                tbone 24 15
+                                tranny 48 10
+                                uma 73 40
+                                grumpkin 42 70
+                                dusty 43 75
+                                grumpy 22 80
+                                eddie 7 20
+                                tory 18 12
+                                sally 4 50
+                                babe 30 10)))
+
+    (def solution (format-dolls '( luke 9 150
+                                   anthony 13 35
+                                   candice 153 200
+                                   dorothy 50 160
+                                   puppy 15 60
+                                   randal 27 60
+                                   marc 11 70
+                                   grumpkin 42 70
+                                   dusty 43 75
+                                   grumpy 22 80
+                                   eddie 7 20
+                                   sally 4 50)))
 
     (def mysolution (knapsack 400 dolls))
+    (print-problem 400 dolls mysolution)
+    (is (= mysolution solution))))
 
-    (println mysolution)                    
+
+
+  (deftest zeroitems-test
+  (testing "Makes sure no items doesn't bomb"
+    (def dolls (format-dolls '( )))
+
+    (def solution (format-dolls '( )))
+
+    (def mysolution (knapsack 400 dolls))
+    (print-problem 400 dolls mysolution)
+    (is (= mysolution solution))))
+
+  
+
+  (deftest zeroweight-test
+  (testing "Makes sure no weight doesn't bomb"
+    (def dolls (format-dolls '( luke 9 150
+                                anthony 13 35
+                                candice 153 200
+                                dorothy 50 160
+                                puppy 15 60
+                                thomas 68 45
+                                randal 27 60
+                                april 39 40
+                                nancy 23 30
+                                bonnie 52 10
+                                marc 11 70
+                                kate 32 30
+                                tbone 24 15
+                                tranny 48 10
+                                uma 73 40
+                                grumpkin 42 70
+                                dusty 43 75
+                                grumpy 22 80
+                                eddie 7 20
+                                tory 18 12
+                                sally 4 50
+                                babe 30 10)))
+
+    (def solution (format-dolls '( )))
+
+    (def mysolution (knapsack 0 dolls))
+    (print-problem 0 dolls mysolution)
     (is (= mysolution solution))))
