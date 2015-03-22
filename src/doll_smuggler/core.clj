cc670143bcb6e76ba4547712f3d73dc68fe66911
Ryan Popa <ryanp@recsolu.com>
2015-03-22 11:33:44 -0500

    Adding some tests

--- src/doll_smuggler/core.clj
+++ src/doll_smuggler/core.clj
@@ -4,17 +4,30 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (println "Test")
+)
 
 (defn m
   "The max value attained with weight less than or equal to w using items up to i"
-  [i w weight v]
+  [i w weight v chosenIndexes]
   (if (= i 0)
     0
-    (if (> (nth w (dec i)) weight) (m (dec i) w weight v)
-      (max (m (dec i) w weight v) (+ (m (dec i) w (- weight (nth w (dec i))) v) (nth v (dec i))))
+    (if (> (nth w (dec i)) weight)
+      (m (dec i) w weight v chosenIndexes)
+      (let [ notThis (m (dec i) w weight v chosenIndexes)
+             this (+ (m (dec i) w (- weight (nth w (dec i))) v chosenIndexes) (nth v (dec i))) ]
+        (if (>= notThis this) notThis
+	   (do (conj chosenIndexes (dec i)) this))
+      )
     )
   )
+)
+
+(defn formatdolls
+  "Turns a list of doll info into a list of dictionaries of dolls"
+  [dolls]
+  (map (fn [[name weight value]] {:name name :weight weight :value value}) (partition 3 dolls))
 )
 
 (defn knapsack
@@ -23,7 +36,8 @@
   (let [dollcount (count dolls)
         weights (map (fn [doll] (doll :weight)) dolls)
         values (map (fn [doll] (doll :value)) dolls)]
-    (m dollcount weights maxWeight values)
+    ;(m dollcount weights maxWeight values)
+    '(test 4 10)
   )
 )
 
--- test/doll_smuggler/core_test.clj
+++ test/doll_smuggler/core_test.clj
@@ -2,6 +2,50 @@
   (:require [clojure.test :refer :all]
             [doll-smuggler.core :refer :all]))
 
+(deftest formatdolls-test
+  (testing "Format doll info into doll list."
+    (def dolls '( luke 9 150
+    		  anthony 13 35))
+    (def formatted '( {:name luke :weight 9 :value 150}
+                      {:name anthony :weight 13 :value 35}))
+    (is (= (formatdolls dolls) formatted))))
+
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (def dolls '( luke 9 150
+  	  	  anthony 13 35
+		  candice 153 200
+   		  dorothy 50 160
+                  puppy 15 60
+                  thomas 68 45
+                  randal 27 60
+                  april 39 40
+                  nancy 23 30
+                  bonnie 52 10
+                  marc 11 70
+                  kate 32 30
+                  tbone 24 15
+                  tranny 48 10
+                  uma 73 40
+                  grumpkin 42 70
+                  dusty 43 75
+                  grumpy 22 80
+                  eddie 7 20
+                  tory 18 12
+                  sally 4 50
+                  babe 30 10))
+
+    (def solution '( sally 4 50
+                     eddie 7 20
+ 		     luke 9 150
+                     marc 11 70
+                     anthony 13 35
+                     puppy 15 60
+		     grumpy 22 80
+		     randal 27 60
+ 		     grumpkin 42 70
+		     dusty 43 75
+ 		     dorothy 50 160
+		     candice 153 200))
+                    
+    (is (= (knapsack 400 dolls) solution))))
